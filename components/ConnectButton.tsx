b8fb51da3b69caca77e83c0ffc8081096b97486b
dblodorn <dain@db13.us>
2022-09-23 11:27:02 -0400

    adjust padding on button wrapper

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -4,7 +4,7 @@ import { Avatar } from './elements/Avatar'
 export function ConnectButton({ ...props }) {
   return (
     <div
-      className="connect-button-wrapper relative flex items-center pr-3 pl-1.5 py-1.5 rounded-2xl bg-slate-100 overflow-hidden"
+      className="connect-button-wrapper relative flex items-center pr-3 pl-3 py-1.5 rounded-2xl bg-slate-100 overflow-hidden"
       {...props}
     >
       <RKConnectButton.Custom>
@@ -16,7 +16,7 @@ export function ConnectButton({ ...props }) {
                   return <button onClick={openConnectModal} className="text-sm">Connect Wallet</button>
                 }
                 if (chain.unsupported) {
-                  return <div className="text-red-400 text-sm">&nbsp;&#x26A0; Wrong Network</div>
+                  return <div className="text-red-400 text-sm">&#x26A0; Wrong Network</div>
                 }
                 return (
                   <button onClick={openAccountModal}>
