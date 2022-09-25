7bd1a6eed206f01b379f61add27cec1afb8cf337
Salief Lewis <salieflewis@gmail.com>
2022-09-25 14:57:06 -0400

    update title + token pass

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ]
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('@tailwindcss/forms')],
 }
