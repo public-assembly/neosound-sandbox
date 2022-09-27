5404a73d3dc485a3f84ff660aa78c0b505aad833
Salief Lewis <salieflewis@gmail.com>
2022-09-26 20:21:27 -0400

    testing branch of a branch

--- pages/curation.tsx
+++ pages/curation.tsx
@@ -1,7 +1,36 @@
+import {
+  useOwnerCurationFunctions,
+  useCurationFunctions,
+} from '@public-assembly/assemble-curation-functions'
+import { useState } from 'react'
 import { NextPage } from 'next'
 
+const curationContractAddress = '0xdb5D0A79d700c5F823b822F97631DbA2d1E31A63'
+
 const Curation: NextPage = () => {
-  return <></>
+  const [updatedTitle, setUpdatedTitle] = useState('')
+  const { updateTitleWrite } = useOwnerCurationFunctions({
+    curationContractAddress,
+    updatedTitle,
+  })
+
+  return (
+    <div className="mx-auto max-w-sm">
+      <div className="flex flex-wrap"></div>
+      <label className="block my-4">
+        <span className="text-gray-700">Update Title</span>
+        <input
+          className="p-2 mt-2 block w-full rounded-md bg-slate-200"
+          onChange={(e) => setUpdatedTitle(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 w-48 text-center p-2 mt-4 rounded-md text-white hover:bg-blue-600"
+          onClick={() => updateTitleWrite?.()}>
+          Update Title
+        </button>
+      </label>
+    </div>
+  )
 }
 
 export default Curation
