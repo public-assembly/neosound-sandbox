ea83c8e1aa2265dd49a60c38ec00e2d5e91b6447
Dain Blodorn Kim <dain@db13.us>
2022-09-28 11:14:51 -0700

    specify correct package in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@public-assembly/zora-editions-audio-minter"
+    "./node_modules/@public-assembly/audio-player-ui"
   ]
 }
