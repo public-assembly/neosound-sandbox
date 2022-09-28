28bb09314d3e7ee8f1776d9280fa5674246c5182
Javier Szyfer <jszynet@gmail.com>
2022-09-28 14:09:20 -0300

    removed other package to test deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import { useEditionQuery } from '@public-assembly/zora-editions-audio-minter'
 import { ViewsToggle } from 'components/ViewsToggle'
 import { GridView } from 'components/views/Grid'
 import { ListView } from 'components/views/List/ListView'
 import { useState } from 'react'
 
 const Home: NextPage = () => {
-  const { data } = useEditionQuery('0x674fb9ed86b847db9aee0a19e9055d5d2c0e6cc4')
   const [view, setView] = useState('list')
 
   return (
